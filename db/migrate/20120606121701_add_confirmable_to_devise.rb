e8cf61a25c8406375282665c29bb0c450e2b259a
Ivaylo Ivanov <ifq@iservice.bg>
2012-06-07 18:11:46 +0300

    Migration Error Fixed

--- db/migrate/20120606121701_add_confirmable_to_devise.rb
+++ db/migrate/20120606121701_add_confirmable_to_devise.rb
@@ -3,7 +3,7 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :people, :confirmation_token, :string
     add_column :people, :confirmed_at,       :datetime
     add_column :people, :confirmation_sent_at , :datetime
-    add_column :users, :unconfirmed_email, :string
+    add_column :people, :unconfirmed_email, :string
 
     add_index  :people, :confirmation_token, :unique => true
   end
